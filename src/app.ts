19032a7a95969af6e5a67c6c2ee274183688f088
poldevy <rostyslavshyian@gmail.com>
2025-04-28 14:51:08 +0200

    first get

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
-
-
+import { globalErrorHandler } from "./controllers/errorController";
 
 export const app = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-
-  res.send("Hello World with TypeScript!");
+  res.status(200).json({
+    message: 'success'
+  })
 });
 
+
+app.use(globalErrorHandler)
